b2a2a7a8a7d32cb0aba2600c1cd5c2bd6e5447cf
reasmeytith <reasmey.tith@douugh.com>
2023-09-03 18:40:08 +1000

    add classifiers

--- src/my_krml_studentid/models/null.py
+++ src/my_krml_studentid/models/null.py
@@ -1,5 +1,6 @@
-import pandas as pd
+from scipy.stats import mode
 import numpy as np
+
 
 class NullRegressor:
     """
@@ -25,7 +26,6 @@ class NullRegressor:
         Perform a fit followed by predict
     """
 
-
     def __init__(self):
         self.y = None
         self.pred_value = None
@@ -34,6 +34,48 @@ class NullRegressor:
     def fit(self, y):
         self.y = y
         self.pred_value = y.mean()
+
+    def predict(self, y):
+        self.preds = np.full((len(y), 1), self.pred_value)
+        return self.preds
+
+    def fit_predict(self, y):
+        self.fit(y)
+        return self.predict(self.y)
+
+
+class NullClassifier:
+    """
+    Class used as baseline model for classification problem
+    ...
+
+    Attributes
+    ----------
+    y : Numpy Array-like
+        Target variable
+    pred_value : Float
+        Value to be used for prediction
+    preds : Numpy Array
+        Predicted array
+
+    Methods
+    -------
+    fit(y)
+        Store the input target variable and calculate the predicted value to be used
+    predict(y)
+        Generate the predictions
+    fit_predict(y)
+        Perform a fit followed by predict
+    """
+
+    def __init__(self):
+        self.y = None
+        self.pred_value = None
+        self.preds = None
+
+    def fit(self, y):
+        self.y = y
+        self.pred_value = mode(y)[0][0]
 
     def predict(self, y):
         self.preds = np.full((len(y), 1), self.pred_value)
